129834886f8d5710876f6e2d4e4058f3d08f7af1
PrzemyslawKepka <przemyslaw.kepka@yahoo.com>
2025-09-25 17:35:45 +0700

    hiding the form, leaving only the map available

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-form_page = st.Page("pg/form.py", title="Property form", icon="🏠")
+# form_page = st.Page("pg/form.py", title="Property form", icon="🏠")
 map_page = st.Page("pg/map.py", title="Property map", icon="🏠")
 
 # Create the navigation
-pg = st.navigation([form_page, map_page])
+# pg = st.navigation([form_page, map_page])
+pg = st.navigation([map_page])
 
 # Run the selected page
 pg.run()
