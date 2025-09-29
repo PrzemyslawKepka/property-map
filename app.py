44329041a9d3ca6f2ee58f5b01b30c854cefc470
PrzemyslawKepka <przemyslaw.kepka@yahoo.com>
2025-09-30 00:35:41 +0700

    back to old name - debugging mobile device app size issue

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 # form_page = st.Page("pg/form.py", title="Property form", icon="🏠")
-map_page = st.Page("pg/map.py", title="Chiang Mai rentals", icon="🏠")
+# map_page = st.Page("pg/map.py", title="Chiang Mai rentals", icon="🏠")
+map_page = st.Page("pg/map.py", title="Property map", icon="🏠")
 
 # Create the navigation
 # pg = st.navigation([form_page, map_page])
